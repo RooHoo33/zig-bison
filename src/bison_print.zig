fffaf9e7892533d071b7ce810fd47d605fcf2b73
Jack Morse <jroo6002@gmail.com>
2026-02-01 10:59:17 -0500

    Print is now working for all types!

--- src/bison_print.zig
+++ src/bison_print.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const Bison = @import("bison_v2.zig");
 const indentSize = 2;
+const nullValue = "null";
 
 fn getPad(gpa: std.mem.Allocator, indent: u8) ![]u8 {
     const pad: []u8 = try gpa.alloc(u8, indent);
@@ -9,20 +10,20 @@ fn getPad(gpa: std.mem.Allocator, indent: u8) ![]u8 {
     }
     return pad;
 }
+
 fn printObject(gpa: std.mem.Allocator, object: Bison.Object, indent: u8) ![]u8 {
     const pad = try getPad(gpa, indent);
     defer gpa.free(pad);
     var stringResult = try std.ArrayList(u8)
         .initCapacity(gpa, 20);
-    try stringResult.appendSlice(gpa, pad);
     try stringResult.append(gpa, '{');
     try stringResult.append(gpa, '\n');
     for (object.entries, 0..) |entry, index| {
-        const entryString = try printObjectEntry(gpa, entry, indent + indentSize);
+        const entryString = try printRootObjectEntry(gpa, entry, indent + indentSize);
         defer gpa.free(entryString);
         try stringResult.appendSlice(gpa, entryString);
-        if(index + 1 < object.entries.len) {
-        try stringResult.append(gpa, ',');
+        if (index + 1 < object.entries.len) {
+            try stringResult.append(gpa, ',');
         }
         try stringResult.append(gpa, '\n');
     }
@@ -30,8 +31,15 @@ fn printObject(gpa: std.mem.Allocator, object: Bison.Object, indent: u8) ![]u8 {
     try stringResult.append(gpa, '}');
     return stringResult.toOwnedSlice(gpa);
 }
+fn printRootObject(gpa: std.mem.Allocator, object: Bison.Object, indent: u8) ![]u8 {
+    const pad = try getPad(gpa, indent);
+    defer gpa.free(pad);
+    const objectString = try printObject(gpa, object, indent);
+    defer gpa.free(objectString);
+    return try std.mem.concat(gpa, u8, &.{pad, objectString});
+}
 
-fn printObjectEntry(gpa: std.mem.Allocator, node: Bison.ObjectEntry, indent: u8) anyerror![]u8 {
+fn printRootObjectEntry(gpa: std.mem.Allocator, node: Bison.ObjectEntry, indent: u8) anyerror![]u8 {
     const pad = try getPad(gpa, indent);
     defer gpa.free(pad);
     const keyString = try std.fmt.allocPrint(gpa, "\"{s}\": ", .{node.name});
@@ -40,7 +48,7 @@ fn printObjectEntry(gpa: std.mem.Allocator, node: Bison.ObjectEntry, indent: u8)
         .initCapacity(gpa, keyString.len * 2);
     try stringResult.appendSlice(gpa, pad);
     try stringResult.appendSlice(gpa, keyString);
-    const valueString = try printValue(gpa, node.value, indent );
+    const valueString = try printValue(gpa, node.value, indent);
     defer gpa.free(valueString);
     try stringResult.appendSlice(gpa, valueString);
     return try stringResult.toOwnedSlice(gpa);
@@ -52,7 +60,10 @@ fn printValue(gpa: std.mem.Allocator, node: Bison.JsonValueType, indent: u8) any
 
     const result = try switch (node) {
         .Int => std.fmt.allocPrint(gpa, "{d}", .{node.Int}),
+        .Float => std.fmt.allocPrint(gpa, "{d}", .{node.Float}),
+        .Boolean => std.fmt.allocPrint(gpa, "{}", .{node.Boolean}),
         .String => std.fmt.allocPrint(gpa, "\"{s}\"", .{node.String}),
+        .Null => gpa.dupe(u8, nullValue),
         .Array => array: {
             var stringResult = try std.ArrayList(u8)
                 .initCapacity(gpa, 20);
@@ -60,7 +71,6 @@ fn printValue(gpa: std.mem.Allocator, node: Bison.JsonValueType, indent: u8) any
             const innerPad = try getPad(gpa, indent + indentSize);
             defer gpa.free(innerPad);
             for (node.Array, 0..) |listValue, index| {
-
                 const result = try printValue(gpa, listValue, indent + indentSize);
                 defer gpa.free(result);
                 if (index != 0) {
@@ -72,9 +82,9 @@ fn printValue(gpa: std.mem.Allocator, node: Bison.JsonValueType, indent: u8) any
             try stringResult.append(gpa, '\n');
             try stringResult.appendSlice(gpa, pad);
             try stringResult.append(gpa, ']');
-            break :array  try stringResult.toOwnedSlice(gpa);
-
+            break :array try stringResult.toOwnedSlice(gpa);
         },
+        .Object => printObject(gpa, node.Object, indent),
         //.Object => blk: {
         //    var stringResult = try std.ArrayList(u8)
         //        .initCapacity(gpa, 20);
@@ -95,7 +105,6 @@ fn printValue(gpa: std.mem.Allocator, node: Bison.JsonValueType, indent: u8) any
         //    try stringResult.appendSlice(gpa, "}");
         //    break :blk try stringResult.toOwnedSlice(gpa);
         //},
-        else => unreachable,
     };
     return result;
     //defer gpa.free(result);
@@ -105,16 +114,22 @@ fn printValue(gpa: std.mem.Allocator, node: Bison.JsonValueType, indent: u8) any
 test "can print object with primitives" {
     const intEntry: Bison.ObjectEntry = .{ .name = "age", .value = Bison.JsonValueType{ .Int = 20 } };
     const stringEntry: Bison.ObjectEntry = .{ .name = "name", .value = Bison.JsonValueType{ .String = "Jack" } };
-    const object = Bison.Object{ .entries = &.{intEntry, stringEntry} };
+    const isCoolEntry: Bison.ObjectEntry = .{ .name = "is_cool", .value = Bison.JsonValueType{ .Boolean = true } };
+    const debtEntry: Bison.ObjectEntry = .{ .name = "debt", .value = Bison.JsonValueType{ .Float = -987.65 } };
+    const nullEntry: Bison.ObjectEntry = .{ .name = "nothing", .value = Bison.JsonValueType{ .Null = {} } };
+    const object = Bison.Object{ .entries = &.{ intEntry, stringEntry, isCoolEntry, debtEntry, nullEntry } };
     const gpa = std.testing.allocator;
-    const result = try printObject(gpa, object, 2);
+    const result = try printRootObject(gpa, object, 2);
     defer gpa.free(result);
-    const expected = 
+    const expected =
         \\  {
         \\    "age": 20,
-        \\    "name": "Jack"
+        \\    "name": "Jack",
+        \\    "is_cool": true,
+        \\    "debt": -987.65,
+        \\    "nothing": null
         \\  }
-        ;
+    ;
     try std.testing.expectEqualStrings(expected, result);
 }
 
@@ -122,15 +137,49 @@ test "can print a list of ints" {
     const intValueOne: Bison.JsonValueType = Bison.JsonValueType{ .Int = 5 };
     const intValueTwo: Bison.JsonValueType = Bison.JsonValueType{ .Int = 2 };
     const array: Bison.ObjectEntry = .{ .name = "ages", .value = Bison.JsonValueType{ .Array = &.{ intValueOne, intValueTwo } } };
+    const object: Bison.Object = .{ .entries = &.{array} };
     const gpa = std.testing.allocator;
-    const result = try printObjectEntry(gpa, array, 2);
+    const result = try printRootObject(gpa, object, 0);
     defer gpa.free(result);
-    const expected = 
+    const expected =
+        \\{
         \\  "ages": [
         \\    5,
         \\    2
         \\  ]
-        ;
+        \\}
+    ;
+    try std.testing.expectEqualStrings(expected, result);
+}
+
+test "can print a complex object node" {
+    const gpa = std.testing.allocator;
+    const numberEntry = Bison.ObjectEntry{ .name = "age", .value = Bison.JsonValueType{ .Int = 234 } };
+    const nameEntry = Bison.ObjectEntry{ .name = "name", .value = Bison.JsonValueType{ .String = "Jack \\\"Jack\\\" Me" } };
+    const randNumbers = Bison.JsonValueType{ .Array = &.{ Bison.JsonValueType{ .Int = 1 }, Bison.JsonValueType{ .Int = 2 }, Bison.JsonValueType{ .Int = 3 } } };
+    const randEntry = Bison.ObjectEntry{ .name = "rand", .value = randNumbers };
+    const nestedObjectValue = Bison.Object{ .entries = &.{ numberEntry, nameEntry, randEntry } };
+
+    const nestedObject = Bison.ObjectEntry{ .name = "value", .value = Bison.JsonValueType{ .Object = nestedObjectValue } };
+    const idEntry = Bison.ObjectEntry{ .name = "id", .value = Bison.JsonValueType{ .String = "abc123" } };
+    const rootObject = Bison.Object{ .entries = &.{ idEntry, nestedObject } } ;
+
+    const result = try printRootObject(gpa, rootObject, 2);
+    defer gpa.free(result);
+    const expected =
+        \\  {
+        \\    "id": "abc123",
+        \\    "value": {
+        \\      "age": 234,
+        \\      "name": "Jack \"Jack\" Me",
+        \\      "rand": [
+        \\        1,
+        \\        2,
+        \\        3
+        \\      ]
+        \\    }
+        \\  }
+    ;
     try std.testing.expectEqualStrings(expected, result);
 }
 
